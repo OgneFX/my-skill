3fde0357e175171097adbe63e77fed98ee328dca
OgneFF <wyrik9324@mail.ru>
2024-11-21 23:43:30 +0300

    Добавил форму перед футером

--- Web automatisation/scripts/sript.js
+++ Web automatisation/scripts/sript.js
@@ -1,13 +1,13 @@
 document.querySelectorAll('.service-button').forEach((button, index) => {
   button.addEventListener('click', () => {
-    // Сброс всех выделений
+    
     document.querySelectorAll('.service-button').forEach(btn => btn.classList.remove('expanded', 'bottom-right'));
 
-    // Добавляем класс 'expanded'
+    
     button.classList.add('expanded');
 
-    // Если это девятая (или последняя) кнопка, добавляем специальный класс
-    if (index === 8) { // Здесь используется индекс 8 для 9-й кнопки
+    
+    if (index === 8) { 
       button.classList.add('bottom-right');
     }
   });
