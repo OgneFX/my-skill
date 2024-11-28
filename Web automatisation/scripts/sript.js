aa872461718c3e5e0b390485b957b404e083d6d5
OgneFF <wyrik9324@mail.ru>
2024-11-28 22:53:33 +0300

    Добавил изменение текста в header

--- Web automatisation/scripts/sript.js
+++ Web automatisation/scripts/sript.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('.service-button');
+const buttons = document.querySelectorAll('.grid-item');
 
 buttons.forEach((button, index) => {
   button.addEventListener('click', (event) => {
@@ -13,17 +13,18 @@ buttons.forEach((button, index) => {
 
 
 document.addEventListener('click', (event) => {
-  const isButton = event.target.closest('.service-button');
+  const isButton = event.target.closest('.grid-item');
   if (!isButton) { 
     buttons.forEach(btn => btn.classList.remove('expanded', 'bottom-right'));
   }
 });
 
-
+// Header анимация
 
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".view-button");
   const image = document.querySelector(".main-image");
+  const textElement = document.querySelector(".b-text");
   const images = [
       "img/camera-tiandy2.png",
       "img/oven-plk.png",
@@ -31,6 +32,16 @@ document.addEventListener("DOMContentLoaded", () => {
       "img/camera-tiandy.png",
       "img/camera-tiandy5.png",
   ];
+
+  const texts = [
+    "Проектирование и установка систем автоматизации и безопасности.",
+    "Текст для изображения 2",
+    "Текст для изображения 3",
+    "Текст для изображения 4",
+    "Текст для изображения 5",
+  ];
+
+
 
   let currentIndex = 0;
   let delay = 5000;
@@ -44,6 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const updateImage = (index) => {
       image.src = images[index];
+      textElement.textContent = texts[index];
   };
 
   const startCycle = () => {
