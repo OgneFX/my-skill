0591661b10df999ed6e8084e61a106f0a8970bd0
OgneFF <wyrik9324@mail.ru>
2024-12-01 21:43:00 +0300

    Работа над интерфейсом

--- Web automatisation/scripts/sript.js
+++ Web automatisation/scripts/sript.js
@@ -25,20 +25,43 @@ document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".view-button");
   const image = document.querySelector(".main-image");
   const textElement = document.querySelector(".b-text");
+  const formImage = document.querySelector(".form-section-img");
+
+ 
   const images = [
-      "img/camera-tiandy2.png",
-      "img/oven-plk.png",
-      "img/wirenboard-plk7.png",
-      "img/camera-tiandy.png",
-      "img/camera-tiandy5.png",
+      "img/header/camera-tiandy2.png",
+      "img/header/fire-protect.png",
+      "img/header/datchyky.png",
+      "img/header/skud.png",
+      "img/header/wirenboard-plk7.png",
+      "img/header/smart-house.png",
+      "img/header/modem.png",
+      "img/header/apc-ups.png",
+      "img/header/electric.png",
+  ];
+
+    const formImages = [
+    "img/header/electric.png",       
+    "img/header/apc-ups.png",  
+    "img/header/modem.png",      
+    "img/header/smart-house.png",          
+    "img/header/skud.png",    
+    "img/header/skud-bolid.png",  
+    "img/header/camera-tiandy2.png",         
+    "img/header/fire-protect.png",         
+    "img/header/wirenboard-plk7.png",   
   ];
 
   const texts = [
-    "Проектирование и установка систем автоматизации и безопасности.",
-    "Текст для изображения 2",
-    "Текст для изображения 3",
-    "Текст для изображения 4",
-    "Текст для изображения 5",
+    "Проектирование, установка и обслуживание систем видеонаблюдения для дома, офиса и бизнеса.",
+    "Монтаж и настройка систем пожарной безопасности и автоматического оповещения.",
+    "Комплексные решения для защиты объектов: сигнализация, датчики движения и контроль доступа.",
+    "Управление доступом на объекты с помощью карт, кодов или биометрии.",
+    "Проектирование и установка систем автоматизации для контроля инженерных процессов.",
+    "Создание интеллектуальных систем управления освещением, климатом и безопасностью вашего дома.",
+    "Настройка беспроводных сетей и сетевой инфраструктуры для стабильного и быстрого соединения.",
+    "Установка и обслуживание ИБП для защиты техники от перебоев с электроэнергией.",
+    "Профессиональный монтаж и обслуживание электрических сетей и оборудования.",
   ];
 
 
@@ -55,6 +78,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const updateImage = (index) => {
       image.src = images[index];
+      formImage.src = formImages[index];
       textElement.textContent = texts[index];
   };
 
